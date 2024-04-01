2178b2f59bed5513272b7a44df9c2df6fd099443
P J Borowiecki <piotr.borowiecki@yahoo.com>
2024-04-01 14:51:26 +0200

    Scaffold auth-related services and controller

--- backend/src/controllers/v1/auth.controller.ts
+++ backend/src/controllers/v1/auth.controller.ts
@@ -1,4 +1,5 @@
 import express, { type Router } from "express"
+import asyncHandler from "express-async-handler"
 
 import * as authService from "../../services/v1/auth.service"
 
@@ -10,6 +11,14 @@ authController.route("/signin").post(authService.signIn)
 
 authController.route("/signout").post(authService.signOut)
 
+authController.route("/users").get(authService.getAllUsers)
+
+authController.route("/users/:id").get(authService.getUserById)
+authController.route("/users/:id").put(authService.updateUserById)
+authController.route("/users/:id").delete(authService.deleteUserById)
+
 authController.route("/current-user").get(authService.getCurrentUser)
+authController.route("/current-user").put(authService.updateCurrentUser)
+authController.route("/current-user").delete(authService.deleteUserById)
 
 export { authController }
--- backend/src/services/v1/auth.service.ts
+++ backend/src/services/v1/auth.service.ts
@@ -1,4 +1,5 @@
 import { type Request, type Response } from "express"
+import { User } from "../../models/v1/user.model"
 
 export function signUp(_request: Request, response: Response): void {
   response.status(201).send({ message: "signUp" })
@@ -14,4 +15,28 @@ export function signOut(_request: Request, response: Response): void {
 
 export function getCurrentUser(_request: Request, response: Response): void {
   response.status(200).send({ message: "getCurrentUser" })
+}
+
+export function updateCurrentUser(_request: Request, response: Response): void {
+  response.status(200).send({ message: "updateCurrentUser" })
+}
+
+export function deleteCurrentUser(_request: Request, response: Response): void {
+  response.status(200).send({ message: "deleteCurrentUser" })
+}
+
+export function getAllUsers(_request: Request, response: Response): void {
+  response.status(200).send({ message: "getAllUsers" })
+}
+
+export function getUserById(_request: Request, response: Response): void {
+  response.status(200).send({ message: "getUserById" })
+}
+
+export function updateUserById(_request: Request, response: Response): void {
+  response.status(200).send({ message: "updateCurrentUser" })
+}
+
+export function deleteUserById(_request: Request, response: Response): void {
+  response.status(200).send({ message: "deleteUserById" })
 }
